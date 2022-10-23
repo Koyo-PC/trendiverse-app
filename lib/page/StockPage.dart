bddfcdcc3da07a7e0a495757ba17e8becc2f34f9
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-10-23 18:59:38 +0900

    update stocked list from file

--- lib/page/StockPage.dart
+++ lib/page/StockPage.dart
@@ -5,7 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:indexed/indexed.dart';
 import 'package:trendiverse/LocalStrage.dart';
 import 'package:trendiverse/data/Position.dart';
 import 'package:trendiverse/data/StockedTrend.dart';
@@ -22,7 +21,6 @@ class StockPage extends SubPageContent {
 
   @override
   Widget build(BuildContext context) {
-    // TODO DB?
     return InteractiveViewer(
       constrained: true,
       minScale: 1.0,
@@ -59,96 +57,83 @@ class _StockedListNotifier extends ChangeNotifier {
       data[id] = StockedTrend(int.parse(id))
         ..position = Position.fromJson(Map<String, dynamic>.from(pos));
     });
+    // print(loadedData);
 
     print("c");
   }
 
-  void moveTrend(int id, Offset delta) {
-    print(data.keys);
-    if (!data.containsKey(id.toString())) return;
+  void moveTrend(String id, Offset delta) {
+    updateTrends();
+    // print(data.values.map((e) => e.position));
+    if (!data.containsKey(id)) return;
     print("z");
-    var movedTrend = data[id.toString()]!;
+    var movedTrend = data[id]!;
     movedTrend.position.x += delta.dx;
     movedTrend.position.y += delta.dy;
 
     LocalStrage().prefs!.setString("trends_stocked",
         jsonEncode(data.map((key, value) => MapEntry(key, value.position))));
+    // print(data.values.map((e) => e.position));
+    print(LocalStrage().prefs!.getString("trends_stocked")!);
 
+    print("az");
     notifyListeners();
+    print("azt");
   }
 }
 
-class _StockPageTile extends ConsumerWidget implements IndexedInterface {
-  static int topIndex = 0;
-
+class _StockPageTile extends ConsumerWidget {
   static final stockedProvider =
       ChangeNotifierProvider((ref) => _StockedListNotifier());
 
-  late StateProvider indexProvider;
-  int _indexcache = 0;
-  StockedTrend stockedTrend;
+  String id;
 
-  _StockPageTile(this.stockedTrend) {
-    var defaultIndex = ++topIndex;
-    indexProvider = StateProvider((ref) => defaultIndex);
-  }
+  _StockPageTile(this.id) {}
 
   // Indexで前後関係を補正しようとしたがうまく動かず現状スパゲッティになってる
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final stockedTrends = ref.watch(stockedProvider);
     final stockedTrendsNotifier = ref.read(stockedProvider.notifier);
-    final index = ref.watch(indexProvider);
-    final indexNotifier = ref.read(indexProvider.notifier);
-    _indexcache = index;
-    return Indexed(
-      index: index,
-      child: Positioned(
-        left: stockedTrend.position.x,
-        top: stockedTrend.position.y,
-        child: GestureDetector(
-          behavior: HitTestBehavior.deferToChild,
-          dragStartBehavior: DragStartBehavior.down,
-          onPanUpdate: (details) {
-            stockedTrendsNotifier.moveTrend(
-                stockedTrend.getId(), details.delta);
-          },
-          onPanStart: (details) {
-            indexNotifier.state = topIndex++;
-            stockedTrends.updateTrends();
-            // TODO: 前に出してくる
-          },
-          onTap: () async {
-            var data = await stockedTrend.getData();
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SubPage(TrendPage(data)),
-              ),
-            );
-          },
-          child: Container(
-            color: Colors.red,
-            padding: const EdgeInsets.all(10),
-            child: FutureBuilder(
-              future: stockedTrend.getName(),
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data!);
-                } else {
-                  return const Text("データ受信中");
-                }
-              },
+    return Positioned(
+      left: stockedTrends.data[id]!.position.x,
+      top: stockedTrends.data[id]!.position.y,
+      child: GestureDetector(
+        behavior: HitTestBehavior.deferToChild,
+        dragStartBehavior: DragStartBehavior.down,
+        onPanUpdate: (details) {
+          stockedTrendsNotifier.moveTrend(id, details.delta);
+        },
+        onPanStart: (details) {
+          // stockedTrends.updateTrends();
+          // TODO: 前に出してくる
+        },
+        onTap: () async {
+          var data = await stockedTrends.data[id]!.getData();
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SubPage(TrendPage(data)),
             ),
+          );
+        },
+        child: Container(
+          color: Colors.red,
+          padding: const EdgeInsets.all(10),
+          child: FutureBuilder(
+            future: stockedTrends.data[id]!.getName(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!);
+              } else {
+                return const Text("データ受信中");
+              }
+            },
           ),
         ),
       ),
     );
   }
-
-  @override
-  // TODO: implement index
-  int get index => _indexcache;
 }
 
 class _StockPageContent extends ConsumerWidget {
@@ -160,13 +145,13 @@ class _StockPageContent extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(rebuilder);
     ref.read(_StockPageTile.stockedProvider).updateTrends();
-    return Indexer(
+    return Stack(
         fit: StackFit.expand,
         children: ref
             .read(_StockPageTile.stockedProvider)
             .data
             .values
-            .map((todo) => _StockPageTile(todo))
+            .map((todo) => _StockPageTile(todo.getId().toString()))
             .toList());
   }
 }
