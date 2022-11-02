cf2918068a7d6b768a54a80853e6e2dca97d9af2
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-11-03 01:09:36 +0900

    fix #20 (issue of many legend)

--- lib/widgets/Graph.dart
+++ lib/widgets/Graph.dart
@@ -49,18 +49,20 @@ class Graph extends StatelessWidget {
                 // X軸
                 primaryXAxis: /*mode == GraphMode.absolute
                     // 絶対表示(日付)の時
-                    ? */DateTimeAxis(
-                        labelStyle: TextStyle(
-                          color: textColor,
-                        ),
-                        dateFormat: DateFormat("MM/dd\nHH:mm"),
-                      )
-                    /*: NumericAxis(
+                    ? */
+                    DateTimeAxis(
+                  labelStyle: TextStyle(
+                    color: textColor,
+                  ),
+                  dateFormat: DateFormat("MM/dd\nHH:mm"),
+                )
+                /*: NumericAxis(
                         labelStyle: TextStyle(
                           color: textColor,
                         ),
                         labelFormat: '{value}日',
-                      )*/,
+                      )*/
+                ,
                 // Y軸
                 primaryYAxis: NumericAxis(
                     labelStyle: TextStyle(
@@ -88,70 +90,78 @@ class Graph extends StatelessWidget {
   }
 
   List<LineSeries<TrendSnapshot, DateTime>> buildTrendSeries(TrendData data) {
-    final List<LineSeries> series = [];
+    // final List<LineSeries> series = [];
+    final LineSeries twitterSeries;
+    final LineSeries aiSeries;
+    final List<TrendSnapshot> twitterDataList = [];
+    final List<TrendSnapshot> aiDataList = [];
     final trendStartTime = data.getHistoryData(0, dataCount: 1)[0].getTime();
     for (int divIndex = 0; divIndex < data.getDividedCount(); divIndex++) {
-      final List<TrendSnapshot> twitterDataList = [];
-      final List<TrendSnapshot> aiDataList = [];
+      final List<TrendSnapshot> twitterDividedDataList = [];
+      final List<TrendSnapshot> aiDividedDataList = [];
       var snapshots = data.getHistoryData(divIndex, dataCount: 500);
 
       for (var snapshot in snapshots) {
         if (snapshot.getSource() == TrendSource.twitter) {
-          twitterDataList.add(snapshot);
+          twitterDividedDataList.add(snapshot);
         } else {
-          aiDataList.add(snapshot);
+          aiDividedDataList.add(snapshot);
         }
       }
-      if (twitterDataList.isNotEmpty && aiDataList.isNotEmpty) {
-        twitterDataList.add(TrendSnapshot(aiDataList[0].getTime(),
-            aiDataList[0].getHotness(), TrendSource.twitter));
-      } else if (twitterDataList.isNotEmpty) {
-        final lastTwitterData = twitterDataList[twitterDataList.length - 1];
-        twitterDataList.add(TrendSnapshot(
+      if (twitterDividedDataList.isNotEmpty && aiDividedDataList.isNotEmpty) {
+        twitterDividedDataList.add(TrendSnapshot(aiDividedDataList[0].getTime(),
+            aiDividedDataList[0].getHotness(), TrendSource.twitter));
+      } else if (twitterDividedDataList.isNotEmpty) {
+        final lastTwitterData =
+            twitterDividedDataList[twitterDividedDataList.length - 1];
+        twitterDividedDataList.add(TrendSnapshot(
             lastTwitterData.getTime().add(const Duration(minutes: 1)),
-            lastTwitterData.getHotness(),
+            0,
             TrendSource.twitter));
-      }else if(aiDataList.isNotEmpty) {
+      } else if (aiDividedDataList.isNotEmpty) {
         // TODO Do nothing?
       }
-      xValueMapper(TrendSnapshot snapshot, _) {
-        if (mode == GraphMode.absolute) {
-          return snapshot.getTime();
-        } else {
-          return DateTime.fromMillisecondsSinceEpoch(snapshot.getTime().difference(trendStartTime).inMilliseconds/*.inSeconds /
-              86400.0*/);
-        }
-      }
+      twitterDataList.addAll(twitterDividedDataList);
+      aiDataList.addAll(aiDividedDataList);
+    }
 
-      yValueMapper(TrendSnapshot snapshot, _) {
-        if (logarithm) {
-          return log(snapshot.getHotness()) * log10e;
-        } else {
-          return snapshot.getHotness() / 1000;
-        }
-      }
-
-      if (twitterDataList.isNotEmpty) {
-        series.add(LineSeries<TrendSnapshot, DateTime>(
-          legendItemText: data.getName(),
-          dataSource: twitterDataList,
-          xValueMapper: xValueMapper,
-          yValueMapper: yValueMapper,
-          color: const HSLColor.fromAHSL(1, 0, 1, .5).toColor(),
-        ));
-      }
-      if (aiDataList.isNotEmpty) {
-        series.add(LineSeries<TrendSnapshot, DateTime>(
-          legendItemText: data.getName() + "(予測)",
-          dataSource: aiDataList,
-          xValueMapper: xValueMapper,
-          yValueMapper: yValueMapper,
-          color: const HSLColor.fromAHSL(1, 0, 1, .85).toColor(),
-        ));
+    xValueMapper(TrendSnapshot snapshot, _) {
+      if (mode == GraphMode.absolute) {
+        return snapshot.getTime();
+      } else {
+        return DateTime.fromMillisecondsSinceEpoch(snapshot
+                .getTime()
+                .difference(trendStartTime)
+                .inMilliseconds /*.inSeconds /
+              86400.0*/
+            );
       }
     }
-    print(series.runtimeType);
-    return series.cast();
+
+    yValueMapper(TrendSnapshot snapshot, _) {
+      if(snapshot.getHotness() == 0) return null;
+      if (logarithm) {
+        return log(snapshot.getHotness()) * log10e;
+      } else {
+        return snapshot.getHotness() / 1000;
+      }
+    }
+
+    twitterSeries = LineSeries<TrendSnapshot, DateTime>(
+      legendItemText: data.getName(),
+      dataSource: twitterDataList,
+      xValueMapper: xValueMapper,
+      yValueMapper: yValueMapper,
+      color: const HSLColor.fromAHSL(1, 0, 1, .5).toColor(),
+    );
+    aiSeries = LineSeries<TrendSnapshot, DateTime>(
+      legendItemText: data.getName() + "(予測)",
+      dataSource: aiDataList,
+      xValueMapper: xValueMapper,
+      yValueMapper: yValueMapper,
+      color: const HSLColor.fromAHSL(1, 0, 1, .85).toColor(),
+    );
+    return [twitterSeries, aiSeries].cast();
   }
 
   List<LineSeries> buildAllTrendSeries(List<TrendData> data) {
@@ -164,114 +174,6 @@ class Graph extends StatelessWidget {
     print(series[0].dataSource);
     return series;
   }
-
-// List<LineSeries> buildTrendSeries(List<TrendData> data) {
-//   if (mode == GraphMode.absolute) {
-//     // 絶対表示(日付)の場合
-//     final List<LineSeries> seriesList = [];
-//     //
-//     final List<LineSeries> twitterDataList = [];
-//     final List<LineSeries> aiDataList = [];
-//
-//
-//     // twitter
-//     for (var d in data) {
-//       for (int dividedIndex = 0; dividedIndex <
-//           d.getDividedCount(); dividedIndex++) {
-//         seriesList.add(LineSeries<TrendSnapshot, DateTime>(
-//           legendItemText: d.getName(),
-//           dataSource: d
-//               .getHistoryData(0, dataCount: 500) // TODO 仮
-//               .where(
-//                   (element) => element.getSource() == TrendSource.twitter)
-//               .toList(),
-//           xValueMapper: (TrendSnapshot snapshot, _) => snapshot.getTime(),
-//           yValueMapper: (TrendSnapshot snapshot, _) =>
-//           logarithm
-//               ? log(snapshot.getHotness()) * log10e
-//               : snapshot.getHotness() / 1000,
-//           color: const HSLColor.fromAHSL(1, 0, 1, .5).toColor(),
-//         ));
-//       }
-//     }
-//     // ai
-//     for (var d in data) {
-//       seriesList.add(
-//         LineSeries<TrendSnapshot, DateTime>(
-//           legendItemText: d.getName() + "(予測)",
-//           dataSource: d
-//               .getHistoryData(0, dataCount: 500) // TODO 仮
-//               .where((element) => element.getSource() == TrendSource.ai)
-//               .toList(),
-//           xValueMapper: (TrendSnapshot snapshot, _) =>
-//               snapshot.getTime(),
-//           yValueMapper: (TrendSnapshot snapshot, _) =>
-//           logarithm
-//               ? log(snapshot.getHotness()) * log10e
-//               : snapshot.getHotness() / 1000,
-//           color: const HSLColor.fromAHSL(1, 0, 1, 0.85).toColor(),
-//         ),
-//       );
-//     }
-//     return seriesList;
-//   } else {
-//     return data.map(
-//           (d) {
-//         var color = HSLColor.fromAHSL(
-//             1, 360 * data.indexOf(d).toDouble() / data.length, 1, .75);
-//         return LineSeries<TrendSnapshot, double>(
-//           legendItemText: d.getName(),
-//           dataSource: d
-//               .getHistoryData(0, dataCount: 500) // TODO 仮
-//               .where(
-//                   (element) => element.getSource() == TrendSource.twitter)
-//               .toList(),
-//           xValueMapper: (TrendSnapshot snapshot, _) =>
-//           snapshot
-//               .getTime()
-//               .difference(d
-//               .getHistoryData(0, dataCount: 1)[0] // TODO 仮
-//               .getTime())
-//               .inSeconds /
-//               86400.0,
-//           yValueMapper: (TrendSnapshot snapshot, _) =>
-//           logarithm
-//               ? log(snapshot.getHotness()) * log10e
-//               : snapshot.getHotness() / 1000,
-//           color: color.toColor(),
-//         );
-//       },
-//     ).toList()
-//       ..addAll(
-//         data.map(
-//               (d) {
-//             var color = HSLColor.fromAHSL(
-//                 1, 360 * data.indexOf(d).toDouble() / data.length, 1, .75);
-//             return LineSeries<TrendSnapshot, double>(
-//               legendItemText: d.getName() + "(予測)",
-//               dataSource: d
-//                   .getHistoryData(0, dataCount: 500) // TODO 仮
-//                   .where((element) => element.getSource() == TrendSource.ai)
-//                   .toList(),
-//               xValueMapper: (TrendSnapshot snapshot, _) =>
-//               snapshot
-//                   .getTime()
-//                   .difference(d
-//                   .getHistoryData(0, dataCount: 1)[0] // TODO 仮
-//                   .getTime())
-//                   .inSeconds /
-//                   86400.0,
-//               yValueMapper: (TrendSnapshot snapshot, _) =>
-//               logarithm
-//                   ? log(snapshot.getHotness()) * log10e
-//                   : snapshot.getHotness() / 1000,
-//               color: color.withLightness(0.25).toColor(),
-//             );
-//           },
-//         ),
-//       );
-//   }
-// }
 }
 
 // absolute: 日付 relative: 経過時間
