2d3289d4823e9398b0144447434122f1147b55b5
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-11-05 00:24:52 +0900

    fix #56

--- lib/page/TrendManagePage.dart
+++ lib/page/TrendManagePage.dart
@@ -32,154 +32,140 @@ class TrendManagePage extends SubPageContent {
               builder: (BuildContext context, WidgetRef ref, Widget? child) {
                 final data = ref.watch(dataProvider);
                 final dataNotifier = ref.read(dataProvider.notifier);
-                return ListView.separated(
-                  itemCount: data.length + 2,
-                  separatorBuilder: (context, index) {
-                    return SizedBox(
-                      height: 12.0,
-                      child: Center(
-                        child: Container(
-                          width: double.infinity,
-                          height: 1,
-                          color: Colors.white24,
-                        ),
-                      ),
-                    );
+                return DragTarget<int>(
+                  onAccept: (item) {
+                    for (var element in data) {
+                      element.remove(item);
+                    }
+                    dataNotifier.state = data
+                        .where((innerList) => innerList.isNotEmpty)
+                        .toList();
                   },
-                  itemBuilder: (context, index) {
-                    return DragTarget<int>(
-                      builder: (context, candidateItems, rejectedItems) {
-                        if (index == data.length) {
-                          // 削除欄
-                          return Container(
-                            width: double.infinity,
-                            height: 100,
-                            decoration: BoxDecoration(
-                              color: Colors.red.withAlpha(64),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.red.withAlpha(64),
-                                  spreadRadius: 1.0,
-                                  blurRadius: 10.0,
-                                ),
-                              ],
-                            ),
-                            child: const Center(
-                              child: Icon(
-                                Icons.delete,
-                                color: Colors.white54,
-                                size: 50,
-                              ),
-                            ),
-                          );
-                        }
-                        if (index == data.length + 1) {
-                          // 追加欄
-                          return MaterialButton(
-                            onPressed: () {
-                              // 検索
-                              showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return SimpleDialog(
-                                    title: TrendSearch(
-                                      (suggestion) {
-                                        data.add([suggestion["id"]]);
-                                        Navigator.pop(context);
-                                        dataNotifier.state = data.toList();
-                                      },
-                                      excludeId: data.expand((x) => x).toList(),
-                                    ),
-                                  );
-                                },
-                              );
-                            },
-                            child: Container(
-                              width: 150,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                color: Colors.blue,
-                                borderRadius:
-                                    BorderRadius.circular(100), // infinity
-                              ),
-                              child: const Center(
-                                child: Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          );
-                        }
-                        // 普通の欄
+                  builder: (context, candidateItems, rejectedItems) {
+                    return ListView.separated(
+                      itemCount: data.length + 1,
+                      separatorBuilder: (context, index) {
                         return SizedBox(
-                          width: double.infinity,
-                          child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: data[index].map(
-                                (e) {
-                                  final content = Container(
-                                    // width: 100,
-                                    height: 40,
-                                    padding: const EdgeInsets.all(10),
-                                    margin:
-                                        const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                                    decoration: BoxDecoration(
-                                      color: AppColor.shadow,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: Center(
-                                      child: FutureBuilder(
-                                        future: TrenDiverseAPI().getName(e),
-                                        builder: (context, snapshot) {
-                                          if (snapshot.hasData) {
-                                            return Text(
-                                              snapshot.data.toString(),
-                                              style: const TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.white,
-                                                decoration: TextDecoration.none,
-                                              ),
-                                            );
-                                          }
-                                          return const CircularProgressIndicator();
-                                        },
-                                      ),
-                                    ),
-                                  );
-                                  return Draggable<int>(
-                                    data: e,
-                                    feedback: content,
-                                    child: content,
-                                    childWhenDragging:
-                                        Opacity(opacity: 0.5, child: content),
-                                  );
-                                },
-                              ).toList(),
+                          height: 12.0,
+                          child: Center(
+                            child: Container(
+                              width: double.infinity,
+                              height: 1,
+                              color: Colors.white24,
                             ),
                           ),
                         );
                       },
-                      onAccept: (item) {
-                        for (var innerList in data) {
-                          innerList.remove(item);
-                        }
-                        if (index == data.length) {
-                          // 削除欄へのドラッグの時
-                          for (var element in data) {
-                            element.remove(item);
-                          }
-                        } else if (index == data.length + 1) {
-                          // 追加欄へのドラッグの時
-                          data.add([item]);
-                        } else {
-                          // 他の欄へのドラッグの時
-                          data[index].add(item);
-                        }
-                        dataNotifier.state = data
-                            .where((innerList) => innerList.isNotEmpty)
-                            .toList();
+                      itemBuilder: (context, index) {
+                        return DragTarget<int>(
+                          builder: (context, candidateItems, rejectedItems) {
+                            if (index == data.length) {
+                              // 追加欄
+                              return MaterialButton(
+                                onPressed: () {
+                                  // 検索
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return SimpleDialog(
+                                        title: TrendSearch(
+                                          (suggestion) {
+                                            data.add([suggestion["id"]]);
+                                            Navigator.pop(context);
+                                            dataNotifier.state = data.toList();
+                                          },
+                                          excludeId:
+                                              data.expand((x) => x).toList(),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                  width: 150,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius:
+                                        BorderRadius.circular(100), // infinity
+                                  ),
+                                  child: const Center(
+                                    child: Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                            // 普通の欄
+                            return SizedBox(
+                              width: double.infinity,
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                child: Row(
+                                  children: data[index].map(
+                                    (e) {
+                                      final content = Container(
+                                        // width: 100,
+                                        height: 40,
+                                        padding: const EdgeInsets.all(10),
+                                        margin: const EdgeInsets.fromLTRB(
+                                            10, 5, 10, 5),
+                                        decoration: BoxDecoration(
+                                          color: AppColor.shadow,
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        child: Center(
+                                          child: FutureBuilder(
+                                            future: TrenDiverseAPI().getName(e),
+                                            builder: (context, snapshot) {
+                                              if (snapshot.hasData) {
+                                                return Text(
+                                                  snapshot.data.toString(),
+                                                  style: const TextStyle(
+                                                    fontSize: 15,
+                                                    color: Colors.white,
+                                                    decoration:
+                                                        TextDecoration.none,
+                                                  ),
+                                                );
+                                              }
+                                              return const CircularProgressIndicator();
+                                            },
+                                          ),
+                                        ),
+                                      );
+                                      return Draggable<int>(
+                                        data: e,
+                                        feedback: content,
+                                        child: content,
+                                        childWhenDragging: Opacity(
+                                            opacity: 0.5, child: content),
+                                      );
+                                    },
+                                  ).toList(),
+                                ),
+                              ),
+                            );
+                          },
+                          onAccept: (item) {
+                            for (var innerList in data) {
+                              innerList.remove(item);
+                            }
+                            if (index == data.length) {
+                              // 追加欄へのドラッグの時
+                              data.add([item]);
+                            } else {
+                              // 他の欄へのドラッグの時
+                              data[index].add(item);
+                            }
+                            dataNotifier.state = data
+                                .where((innerList) => innerList.isNotEmpty)
+                                .toList();
+                          },
+                        );
                       },
                     );
                   },
